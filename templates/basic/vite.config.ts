52528cf22b6d811faaa9d64d3c369086597ca062
Matias Perez <matiasperezpc@hotmail.com>
2025-02-11 12:37:20 -0300

    Add Vercel deployment configuration and prepare script

--- templates/basic/vite.config.ts
+++ templates/basic/vite.config.ts
@@ -1,11 +1,22 @@
 import { reactRouter } from '@react-router/dev/vite'
-import tailwindcss from '@tailwindcss/vite'
 import { defineConfig } from 'vite'
+import tailwindcss from '@tailwindcss/vite'
 import tsconfigPaths from 'vite-tsconfig-paths'
 
-export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+export default defineConfig(({ isSsrBuild, command }) => ({
+  build: {
+    rollupOptions: isSsrBuild
+      ? {
+          input: './server/app.ts',
+        }
+      : undefined,
+  },
+  ssr: {
+    // add here libraries such as basehub, tempus, lenis
+    noExternal: command === 'build' ? true : ['gsap'],
+  },
   server: {
     port: 3000,
   },
-})
+  plugins: [reactRouter(), tailwindcss(), tsconfigPaths()],
+}))
